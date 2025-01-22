d82b2ad3a7fb88a286845f9df5a4bc94640dd08d
John Kyle Desamparo <99082612+theMyle@users.noreply.github.com>
2025-01-22 21:40:54 +0800

    improved setup script

--- setup.ps1
+++ setup.ps1
@@ -1,13 +1,31 @@
-$folder_path = ".\venv\"
 
-if (!(Test-Path -Path $folder_path)) {
-    Write-Host "Creating virtual environment..."
-    python -m venv venv
-    Write-Host "Virtual environment successfully created"
+# check for python installation
+Write-Host "Checking Python Installation..."
+$pythonPath = Get-Command python -ErrorAction SilentlyContinue
+if (!($pythonPath))
+{
+	Write-Host "Error: No python installation found" -ForegroundColor Red
+	exit 1
 }
 
-.\venv\Scripts\pip.exe install -r .\requirements.txt
-Write-Host "Required python packages successfully installed"
+Write-Host "Python Installation Found: " -NoNewline
+$python_version = & python --version
+Write-Host "$python_version`n" -ForegroundColor Green
+
+
+# check for existing virtual environment
+if (!(Test-Path -Path ".\venv\"))
+{
+    Write-Host "Creating virtual environment..."
+    python -m venv venv
+    Write-Host "Virtual environment successfully created`n"
+}
+
+
+Write-Host "Installing python packages..."
+.\venv\Scripts\pip.exe install -r .\requirements.txt > $null
+Write-Host "Required python packages successfully installed`n"
+
 
 Write-Host "Running app..."
 .\run.ps1
